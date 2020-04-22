1d5556c5ead6a927ce63861017f2e94f878f29c6
Tuca Madalin <bristena100@gmail.com>
2020-04-22 17:58:50 +0300

    HealthBar damage pe E

---
+++ Assets/Damage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+
+    public int maxHp = 100;
+    public int currHp;
+
+    public HealthBar healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currHp = maxHp;
+        healthBar.SetMaxHealth(maxHp);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TakeDmg(20);
+            healthBar.SetHealth(currHp);
+        }
+    }
+
+    void TakeDmg(int dmg)
+    {
+        currHp -= dmg;
+    }
+}
