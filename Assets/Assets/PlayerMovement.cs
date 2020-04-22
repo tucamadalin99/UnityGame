664642eb4f329d12f6ba11f69ba89cac677026c2
GeorgeDolofan <dolofan_g@yahoo.com>
2020-04-22 17:47:35 +0300

    sistem tras fara damage

---
+++ Assets/Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+    float horizMove = 0f;
+    bool jump = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+           
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
---
+++ Assets/Assets/SoundManagerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static AudioClip jumpSound;
+    static AudioSource audioSrc;
+
+    void Start()
+    {
+        jumpSound = Resources.Load<AudioClip>("jump");
+        audioSrc = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void PlaySound(string clip)
+    {
+        switch(clip){
+            
+            case "Jump":
+            audioSrc.PlayOneShot(jumpSound);
+            break;
+        }
+    }
+}
